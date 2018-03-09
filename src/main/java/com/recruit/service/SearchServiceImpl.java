11e480fdf53edb18194eac8c8dfcfc90f3d2196f
melius19 <melius19@protonmail.com>
2018-03-09 21:02:16 +0900

    sql statement

--- src/main/java/com/recruit/service/SearchServiceImpl.java
+++ src/main/java/com/recruit/service/SearchServiceImpl.java
@@ -116,3 +116,83 @@ public class SearchServiceImpl implements SearchService {
 		return list;
 	}
 }
+
+// use project;
+//
+// select bno, title, cid, jobgroupid, jobgroupid2, rgbid, rgsid, jobdesc,
+// recruitnum,
+// employstatusid, salaryid, edu, exp, adddesc, period, acceptmethod,
+// recruitform, regdate, viewcnt
+// from tblrecruit;
+//
+// select * from tblrecruit;
+//
+// select * from tblrecruit where employstatusid in (23,24,25) and jobgroupid2
+// in (27,89) and edu in (12);
+//
+// select * from tblrecruit where (rgbid = 'C' and rgsid = 4) or (rgbid = 'I'
+// and rgsid = 4);
+//
+//
+// INSERT INTO tblrecruit(title, cid, jobgroupid, jobgroupid2, rgbid, rgsid,
+// jobdesc, recruitnum, employstatusid, salaryid, edu, exp, adddesc, period,
+// acceptmethod, recruitform, viewcnt)
+// VALUES( 'MELYCODE(1)', 'SAMSUNG', 1, 19, 'A', 1, '담당업무', 3, 20, 7, 9, 5,
+// '상세모집내용', '접수기간', '접수방법', '이력서양식', 0);
+//
+// INSERT INTO tblrecruit(title, cid, jobgroupid, jobgroupid2, rgbid, rgsid,
+// jobdesc, recruitnum, employstatusid, salaryid, edu, exp, adddesc, period,
+// acceptmethod, recruitform, viewcnt)
+// VALUES( 'MELYCODE(2)', 'SAMSUNG', 1, 20, 'A', 2, '담당업무', 3, 21, 7, 10, 2,
+// '상세모집내용', '접수기간', '접수방법', '이력서양식', 0);
+//
+// INSERT INTO tblrecruit(title, cid, jobgroupid, jobgroupid2, rgbid, rgsid,
+// jobdesc, recruitnum, employstatusid, salaryid, edu, exp, adddesc, period,
+// acceptmethod, recruitform, viewcnt)
+// VALUES( 'MELYCODE(3)', 'SAMSUNG', 1, 19, 'O', 5, '담당업무', 3, 22, 7, 11, 3,
+// '상세모집내용', '접수기간', '접수방법', '이력서양식', 0);
+//
+// INSERT INTO tblrecruit(title, cid, jobgroupid, jobgroupid2, rgbid, rgsid,
+// jobdesc, recruitnum, employstatusid, salaryid, edu, exp, adddesc, period,
+// acceptmethod, recruitform, viewcnt)
+// VALUES( 'MELYCODE(4)', 'SAMSUNG', 1, 20, 'O', 2, '담당업무', 3, 23, 7, 12, 4,
+// '상세모집내용', '접수기간', '접수방법', '이력서양식', 0);
+//
+// INSERT INTO tblrecruit(title, cid, jobgroupid, jobgroupid2, rgbid, rgsid,
+// jobdesc, recruitnum, employstatusid, salaryid, edu, exp, adddesc, period,
+// acceptmethod, recruitform, viewcnt)
+// VALUES( 'MELYCODE(5)', 'SAMSUNG', 1, 19, 'A', 3, '담당업무', 3, 20, 7, 9, 5,
+// '상세모집내용', '접수기간', '접수방법', '이력서양식', 0);
+//
+// INSERT INTO tblrecruit(title, cid, jobgroupid, jobgroupid2, rgbid, rgsid,
+// jobdesc, recruitnum, employstatusid, salaryid, edu, exp, adddesc, period,
+// acceptmethod, recruitform, viewcnt)
+// VALUES( 'MELYCODE(6)', 'SAMSUNG', 1, 20, 'A', 4, '담당업무', 3, 21, 7, 10, 2,
+// '상세모집내용', '접수기간', '접수방법', '이력서양식', 0);
+//
+// INSERT INTO tblrecruit(title, cid, jobgroupid, jobgroupid2, rgbid, rgsid,
+// jobdesc, recruitnum, employstatusid, salaryid, edu, exp, adddesc, period,
+// acceptmethod, recruitform, viewcnt)
+// VALUES( 'MELYCODE(7)', 'SAMSUNG', 1, 19, 'O', 8, '담당업무', 3, 22, 7, 11, 3,
+// '상세모집내용', '접수기간', '접수방법', '이력서양식', 0);
+//
+// INSERT INTO tblrecruit(title, cid, jobgroupid, jobgroupid2, rgbid, rgsid,
+// jobdesc, recruitnum, employstatusid, salaryid, edu, exp, adddesc, period,
+// acceptmethod, recruitform, viewcnt)
+// VALUES( 'MELYCODE(8)', 'SAMSUNG', 1, 20, 'O', 6, '담당업무', 3, 23, 7, 12, 4,
+// '상세모집내용', '접수기간', '접수방법', '이력서양식', 0);
+//
+//
+// select * from tblcode;
+//
+// select * from tblrecruit;
+//
+// select bno, title, cid, jobgroupid, jobgroupid2, rgbid, rgsid, jobdesc,
+// recruitnum, employstatusid, salaryid,
+// edu, exp, adddesc, period, acceptmethod, recruitform, regdate, viewcnt from
+// tblrecruit
+// #where jobgroupid2 = '19' or jobgroupid2 = '20';
+// #where (rgbid = 'O' and rgsid = 5) or (rgbid = 'O' and rgsid = 2);
+// where employstatusid = 4;
+//
+// delete from tblrecruit where title like 'MELY%';
