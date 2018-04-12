9a1310a4a98f56f4989e109dbe7f82094e3ad95a
melius102 <melius102@gmail.com>
2018-04-12 10:52:56 +0900

    www.jobkorea.co.kr Ajax

--- src/main/java/com/recruit/controller/HomeController.java
+++ src/main/java/com/recruit/controller/HomeController.java
@@ -50,7 +50,7 @@ public class HomeController {
 		String url = domain + "/zf_user/search/recruit/page/" + page + "?searchword=" + searchword
 				+ "&searchType=search&order=relation";
 		Document doc = Jsoup.connect(url).get();
-		System.out.println(doc.title());
+		System.out.println(domain);
 		Elements recruitNum = doc.select("div.title_search_area .numcase");
 		System.out.println("recruitNum: " + recruitNum.get(0).text());
 
@@ -63,30 +63,11 @@ public class HomeController {
 			// System.out.println("recruit0: " + recruit);
 		}
 
-		// www.jobkorea.co.kr
-		domain = "http://www.jobkorea.co.kr";
-		url = domain + "/Search/?stext=" + searchword;
-		doc = Jsoup.connect(url).get();
-		System.out.println();
-		System.out.println(doc.title());
-		recruitNum = doc.select("#smGiList .devTotalCount");
-		System.out.println("recruitNum: " + recruitNum.get(0).text());
-
-		recruits = doc.select("#smGiList ul.detailList li");
-		// System.out.println(recruits);
-		for (Element recruit : recruits) {
-			System.out.println("cname :  " + recruit.getElementsByTag("a").get(0).text());
-			System.out.println("href  :  " + domain + recruit.getElementsByTag("a").get(0).attr("href"));
-			System.out.println("title :  " + recruit.getElementsByTag("a").get(1).text());
-			System.out.println("href  :  " + domain + recruit.getElementsByTag("a").get(1).attr("href"));
-			System.out.println();
-		}
-
 		// www.incruit.com
 		url = "http://search.incruit.com/list/search.asp?col=job&il=y&kw=" + searchword + "&startno=0";
 		doc = Jsoup.connect(url).get();
 		System.out.println();
-		System.out.println(doc.title());
+		System.out.println(domain);
 		recruitNum = doc.select(".numall");
 		System.out.println("recruitNum: " + recruitNum.get(0).text());
 
@@ -98,6 +79,24 @@ public class HomeController {
 			System.out.println("title :  " + recruit.getElementsByTag("a").get(1).text());
 			System.out.println("href  :  " + recruit.getElementsByTag("a").get(1).attr("href"));
 			System.out.println();
+			System.out.println();
+		}
+		
+		// www.jobkorea.co.kr
+		domain = "http://www.jobkorea.co.kr";
+		url = domain + "/Search/AjaxSearchList";
+		doc = Jsoup.connect(url).data("stext","java","pageSize","10","page","2").post();
+		System.out.println();
+		System.out.println(domain);
+		recruitNum = doc.select("#smGiList .devTotalCount");
+		System.out.println("recruitNum: " + recruitNum.get(0).text());
+		recruits = doc.select("#smGiList ul.detailList li");
+		// System.out.println(recruits);
+		for (Element recruit : recruits) {
+			System.out.println("cname :  " + recruit.getElementsByTag("a").get(0).text());
+			System.out.println("href  :  " + domain + recruit.getElementsByTag("a").get(0).attr("href"));
+			System.out.println("title :  " + recruit.getElementsByTag("a").get(1).text());
+			System.out.println("href  :  " + domain + recruit.getElementsByTag("a").get(1).attr("href"));
 			System.out.println();
 		}
 		return "home";
